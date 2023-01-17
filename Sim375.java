8c192f637c0096c82c33d040f773de57d62b8c94
VilakshanKh <31506996+Vilakshankh@users.noreply.github.com>
2023-01-17 00:17:28 -0330

    broken: implementing rules of life

--- Sim375.java
+++ Sim375.java
@@ -20,59 +20,99 @@
 
  public class Sim375
  {
-     private int size = 120;            // x-by-y grid of cells
-     //private int magnification = 6;  // pixel-width of each cell
+    private int size = 120;            // x-by-y grid of cell
+    private int[][] board, nextBoard;      // intital board
+// picture to be drawn on screen
+    private JFrame frame; 
+            
 
-     private int[][] board;      // intital board
-     private int[][] tempBoard;  // temp board
+        //life status variables
+    private int alive = 1;
+        //private int dead = 0;
 
-
-    // picture to be drawn on screen
-     
-     private JFrame frame; 
-           
-    
-     //life status variables
-     private int alive = 1;
-     //private int dead = 0;
-
-     //pattern type
-     private char random = 'R';
-     private char tumbler = 'T';
-     private char loafer = 'R';
+    //pattern type
+    private char random = 'R';
+    private char tumbler = 'T';
+    private char loafer = 'R';
 
 
-     private int iterations;
-     Random lifeStatus = new Random();
+    private int iterations;
+    private Random lifeStatus = new Random();
     
     // add parameteres: int iterations, char pattern
-     public Sim375()
-     {
+    public Sim375()
+    {
         //this.pattern = pattern;
         //this.iterations = iterations;
         board = new int[size][size];
-        tempBoard = new int[size][size];
+        nextBoard = new int[size][size];
         frame = new JFrame("Game of Life");
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-     }
-    
+    }
+
      
 
     /* function: initial board 2d array with random life status of each cell
     *   need to do: need to implement initial board setting with different patterns
     */
-    private void InitBoard(int size)
+
+    private void rulesOfLife(int[][] board, int[][] nextBoard, int x, int y, int neighbors)
+    {
+        if      ((board[x][y] == 1) && (neighbors <  2)) nextBoard[x][y] = 0;
+        else if ((board[x][y] == 1) && (neighbors >  3)) nextBoard[x][y] = 0;
+        else if ((board[x][y] == 0) && (neighbors == 3)) nextBoard[x][y] = 1;
+        else nextBoard[x][y] = board[x][y];
+    }
+
+    private int[][] InitBoard(int size)
     {   
+
         for (int x = 0; x < size; x++) 
         {
             for (int y = 0; y < size; y++) 
             {
-              board[x][y] = lifeStatus.nextInt(2);
-              
+                board[x][y] = lifeStatus.nextInt(2);
+            }
+        }
+        return board;
+    }
+    private void NextBoard(int size, int[][] board)
+    {
+       
+        for (int x = 1; x < size-1; x++) {
+            for (int y = 1; y < size-1; y++) {
+        
+        
+                int neighbors = 0;
+                for (int i = -1; i <= 1; i++) {
+                    for (int j = -1; j <= 1; j++) {
+                        neighbors += board[x+i][y+j];
+                    }
+                }
+        
+                neighbors -= board[x][y];
+                rulesOfLife(board, nextBoard, x, y, neighbors);
             }
         }
         
+        board = nextBoard;
+    }
+
+    private void Patterns(char pattern)
+    {
+        if (pattern == 'R')
+        {
+            //InitBoard(pattern, pattern);
+        } 
+        else if(pattern == 'T')
+        {
+
+        }
+        else if(pattern == 'L')
+        {
+
+        }
     }
 
     /* life status of specific cell in board checker*/
@@ -95,11 +135,9 @@
         if(lifeStatus(x,y) == 1){return CAlive;}
         else  {return CDead;}
     }
-     // display (or update) the picture on screen
-     public void displayBoard()
-     {
-        InitBoard(size);
-        frame.setLayout(new GridLayout(size,size));
+
+    private void createPixel(int size, JFrame frame)
+    {
         for (int x = 0; x < size; x++) 
         {
             for(int y = 0; y < size; y++)
@@ -109,14 +147,27 @@
                 pixel.setBackground(colorCell(x,y));
                 frame.add(pixel);
             }
-            
         }
-        //add pixels to frame
+    }
+
+
+     // display (or update) the picture on screen
+    public void displayBoard()
+    {
+        
+        frame.setLayout(new GridLayout(size,size));
+
+        for(int c = 0; c <= 500;c++){
+            NextBoard(size, InitBoard(size));
+            createPixel(size,frame);
+        }
+
+
 
         
         frame.setSize(600,600);
         frame.setVisible(true);
-     }
+    }
  
 
      // must provide grid size (x & y) and level of magnification (m)
