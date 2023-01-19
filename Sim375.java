f4d08f0e770fd4cd28149796bce26a62a198846d
VilakshanKh <31506996+Vilakshankh@users.noreply.github.com>
2023-01-19 01:37:38 -0330

    Implemented different patterns

--- Sim375.java
+++ Sim375.java
@@ -20,30 +20,27 @@
 
  public class Sim375
  {
-     private int size = 120;            // x-by-y grid of cells
-     //private int magnification = 6;  // pixel-width of each cell
+    private int size = 120;            // x-by-y grid of cells
+    //private int magnification = 6;  // pixel-width of each cell
 
-     private int[][] board;      // intital board
-     private int[][] tempBoard;  // temp board
+    private int[][] board;      // intital board
+    private int[][] tempBoard;  // temp board
 
 
     // picture to be drawn on screen
      
-     private JFrame frame; 
-           
-    
-     //life status variables
-     private int alive = 1;
-     //private int dead = 0;
+    private JFrame frame; 
+        
 
-     //pattern type
-     private char random = 'R';
-     private char tumbler = 'T';
-     private char loafer = 'R';
+    //life status variables
+    private int alive = 1;
+    //private int dead = 0;
 
 
-     private int iterations;
-     Random lifeStatus = new Random();
+
+
+    private int iterations;
+    Random alive_dead = new Random();
     
     // add parameteres: int iterations, char pattern
      public Sim375()
@@ -62,18 +59,86 @@
     /* function: initial board 2d array with random life status of each cell
     *   need to do: need to implement initial board setting with different patterns
     */
-    private void InitBoard(int size)
+    private void InitBoard(int size, char pattern)
     {   
-        for (int x = 0; x < size; x++) 
+        if (pattern == 'R')
         {
-            for (int y = 0; y < size; y++) 
+            for (int x = 0; x < size; x++) 
             {
-              board[x][y] = lifeStatus.nextInt(2);
-              
-            }
+                for (int y = 0; y < size; y++) {board[x][y] = alive_dead.nextInt(2);}
+            } 
         }
         
+
+        if (pattern == 'T')
+        {
+            for (int x = 0; x < size; x++) 
+            {
+                for (int y = 0; y < size; y++) {board[x][y] = 0;}
+            }
+            
+            board[1][2] = 1;
+            board[1][8] = 1;
+
+            board[2][1] = 1;
+            board[2][3] = 1;
+            board[2][7] = 1;
+            board[2][9] = 1;
+
+            board[3][1] = 1;
+            board[3][4] = 1;
+            board[3][6] = 1;
+            board[3][9] = 1;
+            
+            board[4][3] = 1;
+            board[4][7] = 1;
+
+            board[5][3] = 1;
+            board[5][4] = 1;
+            board[5][6] = 1;
+            board[5][7] = 1;
+        }
+
+        if (pattern == 'L')
+        {
+            for (int x = 0; x < size; x++) 
+            {
+                for (int y = 0; y < size; y++) {board[x][y] = 0;}
+            }
+
+            board[1][2] = 1;
+            board[1][3] = 1;
+            board[1][6] = 1;
+            board[1][8] = 1;
+            board[1][9] = 1;
+
+            board[2][1] = 1;
+            board[2][4] = 1;
+            board[2][7] = 1;
+            board[2][8] = 1;
+
+            board[3][2] = 1;
+            board[3][4] = 1;
+
+            board[4][3] = 1;
+
+            board[5][9] = 1;
+
+            board[6][7] = 1;
+            board[6][8] = 1;
+            board[6][9] = 1;
+
+            board[7][6] = 1;
+
+            board[8][7] = 1;
+
+            board[9][8] = 1;
+            board[9][9] = 1;
+        }
+
     }
+
+
 
     /* life status of specific cell in board checker*/
     private int lifeStatus(int x, int y)
@@ -81,6 +146,7 @@
         if (board[x][y] == alive) {return 1;}
         else {return 0;}
     }
+
 
     /* colors cell of coordinate xy of board
      * white or black depending on its life status
@@ -95,10 +161,15 @@
         if(lifeStatus(x,y) == 1){return CAlive;}
         else  {return CDead;}
     }
+
+
+
+
+
      // display (or update) the picture on screen
-     public void displayBoard()
-     {
-        InitBoard(size);
+    public void displayBoard()
+    {
+        InitBoard(size, 'L');
         frame.setLayout(new GridLayout(size,size));
         for (int x = 0; x < size; x++) 
         {
@@ -113,10 +184,10 @@
         }
         //add pixels to frame
 
-        
+    
         frame.setSize(600,600);
         frame.setVisible(true);
-     }
+    }
  
 
      // must provide grid size (x & y) and level of magnification (m)
