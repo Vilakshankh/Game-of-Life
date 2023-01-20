6215d15dee29e715f22053627e0edbf90c59875c
VilakshanKh <31506996+Vilakshankh@users.noreply.github.com>
2023-01-19 22:40:48 -0330

    implemented patterns and neighbors

--- Sim375.java
+++ Sim375.java
@@ -15,32 +15,21 @@
 
  import java.awt.*;
  import java.util.Random;
- import javax.swing.*;
+import java.util.stream.IntStream;
+
+import javax.swing.*;
 
 
  public class Sim375
  {
-    private int size = 120;            // x-by-y grid of cells
-    //private int magnification = 6;  // pixel-width of each cell
-
-    private int[][] board;      // intital board
-    private int[][] tempBoard;  // temp board
-
-
-    // picture to be drawn on screen
-     
+    private int size = 120;           
+    private int[][] board;      
+    private int[][] tempBoard;  
     private JFrame frame; 
-        
-
-    //life status variables
     private int alive = 1;
-    //private int dead = 0;
-
-
-
-
+    private int dead = 0;
     private int iterations;
-    Random alive_dead = new Random();
+    private Random alive_dead = new Random();
     
     // add parameteres: int iterations, char pattern
      public Sim375()
@@ -55,10 +44,6 @@
      }
     
      
-
-    /* function: initial board 2d array with random life status of each cell
-    *   need to do: need to implement initial board setting with different patterns
-    */
     private void InitBoard(int size, char pattern)
     {   
         if (pattern == 'R')
@@ -68,8 +53,7 @@
                 for (int y = 0; y < size; y++) {board[x][y] = alive_dead.nextInt(2);}
             } 
         }
-        
-
+    
         if (pattern == 'T')
         {
             for (int x = 0; x < size; x++) 
@@ -138,35 +122,68 @@
 
     }
 
+    private int getNeighbors(int x, int y)
+    {
+        int aliveNeighbors = 0;
+        for (int i = -1; i<=1; i++)
+        {
+            for (int j = -1; j <= 1; j++)
+            {
+                if (!(i == 0 && j == 0))
+                {
+                    int x_coord = (x + i + size) % size;
+                    int y_coord = (y + j + size) % size;
+                    if (getLifeStatus(x, y)){aliveNeighbors++;}
+                }
+            }
+        }
+        return aliveNeighbors;
+    }
 
-
-    /* life status of specific cell in board checker*/
-    private int lifeStatus(int x, int y)
+    private int getLifeStatus(int x, int y)
     {
         if (board[x][y] == alive) {return 1;}
         else {return 0;}
     }
 
-
-    /* colors cell of coordinate xy of board
-     * white or black depending on its life status
-    */
-    private Color colorCell(int x, int y)
+    private Color getCellColor(int x, int y)
     {
         Color CAlive = new Color(0,0,0);
         Color CDead = new Color(255,255,255);
         
     
         // set() colours an individual pixel
-        if(lifeStatus(x,y) == 1){return CAlive;}
+        if(getLifeStatus(x,y) == 1){return CAlive;}
         else  {return CDead;}
     }
 
 
+    private void update()
+    {
+        for (int x = 0; x < size; x++)
+        {
+            for (int y = 0; y < size; y++)
+            {
+                int aliveNeighbors = getNeighbors(x, y);
+                if (board[x][y] == alive)
+                {
+                    if(aliveNeighbors < 2 || aliveNeighbors > 3){ tempBoard[x][y] = dead;}
+                    else{tempBoard[x][y] = alive;}
+                }
+                else
+                {
+                    if (aliveNeighbors == 3) {tempBoard[x][y] = alive;}
+                    else{tempBoard[x][y] = dead;}
+                }
+            }
+        }
+
+        for(int i = 0; i < size; i++) {System.arraycopy(tempBoard[i], 0, board[i], 0, size);}
+    }
 
 
+    
 
-     // display (or update) the picture on screen
     public void displayBoard()
     {
         InitBoard(size, 'L');
@@ -177,14 +194,11 @@
             {
                 JPanel pixel = new JPanel();
                 //pixel.setSize(100,100);
-                pixel.setBackground(colorCell(x,y));
+                pixel.setBackground(getCellColor(x,y));
                 frame.add(pixel);
             }
             
         }
-        //add pixels to frame
-
-    
         frame.setSize(600,600);
         frame.setVisible(true);
     }
